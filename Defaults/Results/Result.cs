8311009aedd7bd98f0c3248c3b316f6848f77eb0
Zoe Abernathy <zoeabernathy@outlook.com>
2023-11-08 15:03:23 +0100

    Made properties in the Results class readonly

--- Defaults/Results/Result.cs
+++ Defaults/Results/Result.cs
@@ -1,47 +1,38 @@
 ﻿namespace Defaults;
 
-public struct Result<T>
-{
-    public T Value { get; set; } = default!;
-    public Exception Exception { get; set; } = null!;
+public struct Result<T> {
+	public readonly T Value { get; } = default!;
+	public readonly Exception Exception { get; } = null!;
 
-    private readonly bool _stateFailure;
+	private readonly bool _stateFailure;
 
-    public Result(T value)
-    {
-        Value = value;
-        _stateFailure = false;
-    }
+	public Result(T value) {
+		Value = value;
+		_stateFailure = false;
+	}
 
-    public Result(Exception exception)
-    {
-        Exception = exception;
-        _stateFailure = true;
-    }
+	public Result(Exception exception) {
+		Exception = exception;
+		_stateFailure = true;
+	}
 
-    public readonly bool EnsureSuccess() => !_stateFailure;
+	public readonly bool EnsureSuccess() => !_stateFailure;
 
-    public readonly void Handle(Action<T> onSuccess, Action<Exception> onException)
-    {
-        if (_stateFailure)
-        {
-            onException(Exception);
-        }
-        else
-        {
-            onSuccess(Value);
-        }
-    }
+	public readonly void Handle(Action<T> onSuccess, Action<Exception> onException) {
+		if (_stateFailure) {
+			onException(Exception);
+		}
+		else {
+			onSuccess(Value);
+		}
+	}
 
-    public async readonly Task HandleAsync(Func<T, Task> onSuccess, Func<Exception, Task> onException)
-    {
-        if (_stateFailure)
-        {
-            await onException(Exception);
-        }
-        else
-        {
-            await onSuccess(Value);
-        }
-    }
+	public async readonly Task HandleAsync(Func<T, Task> onSuccess, Func<Exception, Task> onException) {
+		if (_stateFailure) {
+			await onException(Exception);
+		}
+		else {
+			await onSuccess(Value);
+		}
+	}
 }
